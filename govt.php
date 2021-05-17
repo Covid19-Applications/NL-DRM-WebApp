7405d00a9c1f920dfd8658d9f4d81d7855139bf1
Shaharyaar7 <shaharyaarkutchi7@gmail.com>
2021-05-17 22:24:59 +0530

    created modules

---
+++ govt.php
@@ -0,0 +1,181 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+<br><br><br><br>
+
+  
+<style type="text/css">
+  
+.button{
+  background: #3498db;
+    color: #fff;
+    border-radius: 50px;
+    margin: 0 15px;
+    padding: 10px 30px; 
+}
+
+#services .box {
+    padding: 50px 20px;
+    margin-bottom: 50px;
+    text-align: center;
+    border: 1px solid #e6e6e6;
+    height: 170px;
+    position: relative;
+    background: #fafafa;
+}
+  
+</style>
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area breadcam_bg ">
+        <div class="container">
+          <br><br>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="bradcam_text">
+                        <img src="img/home/govt1.png" class="img-fluid" style="height: 150px;">
+                        <h1>Government Services</h1>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="bradcam_text">
+                      <br>
+                        <img src="img/nagaland/banner-4.jpg" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+          <h2>Digital Nagaland</h2>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3">
+            <div class="member" data-aos="fade-up">
+              <div class="member-info">
+                <h4><a href="https://www.nagaland.gov.in/portal/portal/StatePortal/UsefulLinks/eServices">Ration Card Applications</a></h4>
+                
+              </div>
+            </div>
+            <div class="member" data-aos="fade-up" style="height: 100px;">
+              <div class="member-info">
+                <h4><a href="https://www.nagaland.gov.in/Nagaland/UsefulLinks/aadhaar_enrolment_correction_form_version_4.0.pdf">Aadhaar Enrollment Form</a></h4>
+              </div>
+            </div>
+          </div> 
+          <div class="col-lg-3"> 
+              <div class="member" data-aos="fade-up"> 
+              <div class="member-info">
+                <h4><a href="http://www.mdoner.gov.in/content/nagaland-4">DONER</a></h4>
+               
+              </div>
+            </div>
+             <div class="member" data-aos="fade-up" >
+              <div class="member-info">
+                <h4><a href="https://www.nagaland.gov.in/portal/portal/StatePortal/UsefulLinks/eServices">Pension Applications</a></h4>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <img src="img/nagaland/banner-1.jpg" class="img-fluid">
+          </div>  
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h3 class="section-title text-center" >Government Rules</h3>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/nagaland/banner-6.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/nagaland/banner-7.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/nagaland/banner-8.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/nagaland/banner-9.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/nagaland/banner-10.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/nagaland/banner-11.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/nagaland/banner-13.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+          
+         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/nagaland/banner-14.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/nagaland/banner-15.jpeg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
